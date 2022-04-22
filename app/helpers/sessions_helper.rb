a8fd296eb97f2344e7cd309ed281f462ecbd4af4
adamaru <adamaru8700@icloud.com>
2022-04-22 10:58:10 +0000

    B1-B7

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,6 @@
 module SessionsHelper
   
+  # 引数に渡されたユーザーオブジェクトでログインします。
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -9,19 +10,21 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-
+  
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
   
+  # セッションと@current_userを破棄します
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
   
+  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -34,6 +37,7 @@ module SessionsHelper
     end
   end
   
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil?
   end
